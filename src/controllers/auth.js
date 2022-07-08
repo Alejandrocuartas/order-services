53a9deb61e87377b77ae4c950c5517dda4c1f8c6
Alejandro Cuartas Marin <cuartasmarin734@gmail.com>
2022-07-07 21:04:17 -0500

    *

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -8,7 +8,7 @@ const googleLogin = async (req = request, res = response) => {
         const token = await googleAuth(googleToken);
         res.status(200)
             .header('Access-Control-Allow-Credentials', true)
-            .header('Access-Control-Allow-Origin', 'https://ordena.netlify.app')
+            .header('Access-Control-Allow-Origin', 'http://localhost:8080')
             .header('access-control-expose-headers', 'Set-Cookie')
             .cookie('userToken', token, {
                 sameSite: 'none',
